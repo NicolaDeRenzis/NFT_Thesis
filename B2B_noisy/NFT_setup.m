4d4eb672d29069ce917bfba9fd31edf48b42f5bc
NicolaDeRenzis <s161172@studnet.dtu.dk>
2018-02-10 11:31:30 +0100

    Added energy check, refined NFT and OSNR parameters

--- B2B_noisy/NFT_setup.m
+++ B2B_noisy/NFT_setup.m
@@ -6,7 +6,7 @@ robolog off
 Fs = 1e12;
 Fc = 193.4e12;
 
-osnr_cycle = 100:-10:10;
+osnr_cycle = 42:-4:14;
 realization = 1e2;
 
 
@@ -23,10 +23,10 @@ nPoints = 2^12; % minimum 2048
 Rs = Fs/nPoints;
 
 %INFT parameters
-param.INFT.Tn        = 0.03/Rs;                              %[km]
+param.INFT.Tn        = 0.15/Rs;                              %[km]
 param.INFT.gamma     = 1.27;                             %[/W/km]
 param.INFT.D         = 17;                            %[ps/(nm km)]
-param.INFT.method    = 'darboux_v2';
+param.INFT.method    = 'darboux_simplified';
 param.INFT.Fc        = Fc;
 param.INFT.nPoints   = nPoints;
 param.INFT.setNFTParameterB = 0;
@@ -37,9 +37,11 @@ param.NFT.gamma     = param.INFT.gamma;                             %[/W/km]
 param.NFT.D         = param.INFT.D;                               %[ps/(nm km)]
 param.NFT.nPoints   = nPoints;
 param.NFT.methodDiscreteSpectrum = 'TrapezFB';
+param.NFT.tolUniqueEigenvalue = 0.05; % sensibility to locating the same eigenvalue
+param.NFT.tolAllEigenvaluesFound = 0.3; % energy threshold to fulfill for terminating the eigs search
 param.NFT.computeDiscreteSpectrumEnabled = 1;
 param.NFT.computeContinuousSpectrumEnabled = 1;
-param.NFT.complexPlaneSearchArea = 1.1 * (max(0.1, max(real(discreteEigenvalues))) + ...
+param.NFT.complexPlaneSearchArea = 3 * (max(0.1, max(real(discreteEigenvalues))) + ...
     1i*max(imag(discreteEigenvalues)));
 param.NFT.mexEnabled = 1;
 param.NFT.returnNFTParameterB = param.INFT.setNFTParameterB;
@@ -52,6 +54,8 @@ inft.normalizationParameters(const.c/Fc);
 inft = DiscreteINFT_v1(param.INFT);
 sigDarb = inft.traverse(discreteEigenvalues, discreteSpectrum, Rs);
 
+nft_out = NFT_v8(param.NFT);
+
 for noise_index = 1:numel(osnr_cycle)
     
     %OSNR parameters
@@ -63,14 +67,9 @@ for noise_index = 1:numel(osnr_cycle)
         % Add noise to the generated signal
         sigNoise = osnr.traverse(sigDarb);
         
-        % Compute NFT until all eignevalues are found. A little random.
-        toten = 0;
-        while toten < 0.9
-            nft_out = NFT_v8(param.NFT);
-            nft_out.traverse(sigNoise);
-            E = nft_out.results.E;
-            toten = (E.Ec + E.Ed)/E.ETot;
-        end
+        % Compute NFT until all eignevalues are found
+        nft_out.traverse(sigNoise);
+        E = nft_out.results.E;
         
         robolog('Discrete eigenvalues OUTPUT signal:');
         egDb(n,:) = nft_out.discreteEigenvalues(); %egDb
@@ -78,10 +77,15 @@ for noise_index = 1:numel(osnr_cycle)
         dsDb(n,:) = nft_out.discreteSpectrum(); %dsDb
         E_cont(n) = E.Ec;
         E_disc(n) = E.Ed;
+        t = genTimeAxisSig(sigNoise,'central');
+        E_sigNoise(n) = trapz(t,abs(get(sigNoise)).^2);
+        tmp{noise_index,n} = sigNoise;
     end
+    store_sigNoise{noise_index} = tmp{noise_index,1};
     % check how energy flows from discrete to continuous spectrum
     E_cont_tot(noise_index) = mean(E_cont);
     E_disc_tot(noise_index) = mean(E_disc);
+    E_sig_tot(noise_index) = mean(E_sigNoise);
     
     figure(1)
     plot(real(egDb),imag(egDb),'b.',real(discreteEigenvalues),imag(discreteEigenvalues),'r.')
@@ -106,18 +110,27 @@ for noise_index = 1:numel(osnr_cycle)
     
 end
 
-%{
+%%{
 %% Display the results
-plotNFTConstellation_v1(egDb, dsDb , 'refEigenvalues', discreteEigenvalues, 'refSpectrum', discreteSpectrum);
+%plotNFTConstellation_v1(egDb, dsDb , 'refEigenvalues', discreteEigenvalues, 'refSpectrum', discreteSpectrum);
 
 figure;
-t = genTimeAxisSig(sigNoise,'central');
+show_osnr_level = 1;
+t = genTimeAxisSig(store_sigNoise{show_osnr_level},'central');
 subplot(2,2,[1,3])
-plot(t,abs(get(sigNoise)).^2,t,abs(get(sigDarb)).^2)
+plot(t,abs(get(store_sigNoise{show_osnr_level})).^2,t,abs(get(sigDarb)).^2)
 xlabel('t')
 ylabel('|x(t)|^2')
 subplot(2,2,[2,4])
-plot(t,angle(get(sigNoise))./pi,t,angle(get(sigDarb))./pi)
+plot(t,angle(get(store_sigNoise{show_osnr_level}))./pi,t,angle(get(sigDarb))./pi)
 xlabel('t')
 ylabel('phase/\pi')
+
+figure;
+plot(osnr_cycle,E_cont_tot,'-o',osnr_cycle,E_disc_tot,'-o',...
+    osnr_cycle,E_cont_tot+E_disc_tot,'k--o',osnr_cycle,E_sig_tot,'k-o');
+xlabel('OSNR [dB]')
+ylabel('Signal Energy [J]')
+legend('Continuous spectrum','Discrete Spectrum',...
+    'Total NFT enegry','Total signal energy','Location','Northwest')
 %}
